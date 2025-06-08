f34183d76243954b7a51ee6018cab94239efe578
RomanDanyl <162486585+RomanDanyl@users.noreply.github.com>
2025-06-08 19:32:08 +0300

    Add settings for sending emails

--- gloryphonics/settings.py
+++ gloryphonics/settings.py
@@ -127,3 +127,12 @@ AUTH_USER_MODEL = "user.User"
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = "gloryphonic@gmail.com"
+EMAIL_HOST_PASSWORD = "jeprntjknqnhfwzb"
+DEFAULT_FROM_EMAIL = "Gloryphonic Site <gloryphonic@gmail.com>"
+# SITE_URL = "https://mixelio.github.io/DAH/#"
