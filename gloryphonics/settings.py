3ac8bdcb52aa3731c9f64163501990343d931ed3
RomanDanyl <162486585+RomanDanyl@users.noreply.github.com>
2025-06-17 17:54:55 +0300

    Add 18.197.239.62 to allowed hosts

--- gloryphonics/settings.py
+++ gloryphonics/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "18.197.239.62"]
 
 
 # Application definition
