8b737b58192b6831e086618e9624e93166de8bff
RomanDanyl <162486585+RomanDanyl@users.noreply.github.com>
2025-06-20 22:37:56 +0300

    Delete duplicates

--- user/migrations/user/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from user.models import User, UserImage, RegistrationApplication
-
-admin.site.register(User)
-admin.site.register(UserImage)
-admin.site.register(RegistrationApplication)
