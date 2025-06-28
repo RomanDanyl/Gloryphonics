dc6c6044139876fa4729904099880570486cb284
RomanDanyl <162486585+RomanDanyl@users.noreply.github.com>
2025-06-28 15:07:03 +0300

    Split settings to production and development environments

---
+++ gloryphonics/settings/prod.py
@@ -0,0 +1,21 @@
+from .base import *
+
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "18.197.239.62", "gloryphonic-api.ddns.net"]
+
+# Database
+# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("PG_NAME"),
+        "USER": os.getenv("PG_USER"),
+        "PASSWORD": os.getenv("PG_PASSWORD"),
+        "HOST": os.getenv("PG_HOST"),
+        "PORT": os.getenv("PG_PORT"),
+    }
+}
