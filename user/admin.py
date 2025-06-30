cf84c3579149d3fe17d6b0c36d6e79e238b54c8e
RomanDanyl <162486585+RomanDanyl@users.noreply.github.com>
2025-06-30 17:45:26 +0300

    Register Album & Follower models in admin panel

--- user/admin.py
+++ user/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
-from user.models import User, UserImage, RegistrationApplication
+from user.models import User, UserImage, RegistrationApplication, Follower, Album
 
 admin.site.register(User)
 admin.site.register(UserImage)
 admin.site.register(RegistrationApplication)
+admin.site.register(Follower)
+admin.site.register(Album)
