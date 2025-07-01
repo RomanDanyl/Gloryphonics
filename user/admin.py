900ca0dc4c67b0c24c1d0954f6860ae5ac91ec86
RomanDanyl <162486585+RomanDanyl@users.noreply.github.com>
2025-07-01 15:20:52 +0300

    Register SocialLinks model in admin panel

--- user/admin.py
+++ user/admin.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
 
-from user.models import User, UserImage, RegistrationApplication, Follower, Album
+from user.models import (
+    User,
+    UserImage,
+    RegistrationApplication,
+    Follower,
+    Album,
+    SocialLinks,
+)
 
 admin.site.register(User)
 admin.site.register(UserImage)
 admin.site.register(RegistrationApplication)
 admin.site.register(Follower)
 admin.site.register(Album)
+admin.site.register(SocialLinks)
