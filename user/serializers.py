b169050f164f05ad5b5d9a25f7ed63c7b67113b4
RomanDanyl <162486585+RomanDanyl@users.noreply.github.com>
2025-07-08 11:47:37 +0300

    Add AuthTokenSerializer

--- user/serializers.py
+++ user/serializers.py
@@ -1,8 +1,9 @@
-from typing import Optional
+from typing import Optional, Dict, Any
 
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, authenticate
 from django.contrib.auth.tokens import PasswordResetTokenGenerator
 from django.core.files.uploadedfile import UploadedFile
+from django.utils.translation import gettext as _
 from django.utils.http import urlsafe_base64_decode
 from rest_framework import serializers
 
@@ -254,3 +255,38 @@ class UserRetrieveSerializer(UserListSerializer):
             "followers",
             "images",
         )
+
+
+class AuthTokenSerializer(serializers.Serializer):
+    email = serializers.CharField(label=_("Email"), write_only=True)
+    password = serializers.CharField(
+        label=_("Password"),
+        style={"input_type": "password"},
+        trim_whitespace=False,
+        write_only=True,
+    )
+    token = serializers.CharField(label=_("Token"), read_only=True)
+
+    def validate(self, attrs: Dict[str, Any]) -> Dict[str, Any]:
+        email = attrs.get("email")
+        password = attrs.get("password")
+
+        if email and password:
+            user = authenticate(
+                request=self.context.get("request"),
+                email=email,
+                password=password,
+            )
+
+            # The authenticate call simply returns None for is_active=False
+            # users. (Assuming the default ModelBackend authentication
+            # backend.)
+            if not user:
+                msg = _("Unable to log in with provided credentials.")
+                raise serializers.ValidationError(msg, code="authorization")
+        else:
+            msg = _('Must include "email" and "password".')
+            raise serializers.ValidationError(msg, code="authorization")
+
+        attrs["users"] = user
+        return attrs
