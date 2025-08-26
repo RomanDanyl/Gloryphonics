bf4ec5e356c0c481badc3e5367f96e730671b504
RomanDanyl <162486585+RomanDanyl@users.noreply.github.com>
2025-08-26 14:33:16 +0300

    Add IsManagerOrAdminOrReadOnly to permissions.py

--- user/permissions.py
+++ user/permissions.py
@@ -1,6 +1,8 @@
 from rest_framework.permissions import BasePermission, SAFE_METHODS
 from rest_framework.request import Request
 
+from user.models import User
+
 
 class IsOwnerOrAdminOrReadOnly(BasePermission):
     """
@@ -12,5 +14,23 @@ class IsOwnerOrAdminOrReadOnly(BasePermission):
         self, request: Request, view: object, obj: object
     ) -> bool:
         if request.method in SAFE_METHODS or request.user.is_staff:
+            return True
+        return obj.user == request.user
+
+
+class IsManagerOrAdminOrReadOnly(BasePermission):
+    """
+    Read permission for everyone, but write,
+    update, or delete permissions only for the owner.
+    """
+
+    def has_object_permission(
+        self, request: Request, view: object, obj: object
+    ) -> bool:
+        if (
+            request.method in SAFE_METHODS
+            or request.user.is_staff
+            or request.user.role in [User.RoleChoices.ADMIN, User.RoleChoices.MANAGER]
+        ):
             return True
         return obj.user == request.user
