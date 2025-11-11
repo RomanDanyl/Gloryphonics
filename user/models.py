af3eeeb2cca4939fe28e1912957925c307e20321
RomanDanyl <162486585+RomanDanyl@users.noreply.github.com>
2025-11-11 18:49:22 +0200

    Change relation in Comment model from User to Band & migrations

--- user/models.py
+++ user/models.py
@@ -143,7 +143,7 @@ class SocialLinks(models.Model):
 
 
 class Comment(models.Model):
-    group = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
+    group = models.ForeignKey(Band, on_delete=models.CASCADE, related_name="comments")
     text = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     user = models.CharField(max_length=255)
